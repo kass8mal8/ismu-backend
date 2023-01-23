03e06a93859be3a502f00b23e2a45538f98e653e
kass8mal8 <kassimaly21@gmail.com>
2023-01-23 09:02:19 +0300

    modified:   controllers/userController.js 	modified:   routes/users.js

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,10 +1,10 @@
-const User = require('../model/members')
+const Member = require('../model/members')
 
 const post_member = async(req, res) => {
     const { name, phone, admission } = req.body
 
     try {
-        const member = await User.create({
+        const member = await Member.create({
             name, phone, admission
         })
         res.send(member)
@@ -13,6 +13,17 @@ const post_member = async(req, res) => {
     }
 }
 
+
+const get_member = async(req, res) => {
+    try {
+       const data = await Member.find()
+       res.send(data)
+    } catch (error) {
+        res.status(500).send(error)
+    }
+}
+
 module.exports = {
-    post_member
+    post_member,
+    get_member
 }
--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,11 @@
 const { Router } = require('express')
-const { post_member } = require('../controllers/userController')
+const { post_member, get_member } = require('../controllers/userController')
 const router = Router()
-
-//router.use(express.json())
 
 // POST a new member
 router.post('/', post_member)
+
+// Get all members
+router.get('/', get_member)
 
 module.exports = router
